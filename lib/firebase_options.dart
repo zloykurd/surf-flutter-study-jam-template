ff20d6d07fd65149da0d3e0db7c0c0c11215153c
Rizvan Adzhoyan <radzhoyan@gmail.com>
2022-02-20 17:57:00 +0600

    update firebase creds

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -37,29 +37,29 @@ class DefaultFirebaseOptions {
 
   static FirebaseOptions web(String apiKey) => FirebaseOptions(
         apiKey: apiKey,
-        appId: '1:733105866417:web:c5e6c12528326f5f42c490',
-        messagingSenderId: '733105866417',
-        projectId: 'flutter-study-jam-chat',
-        authDomain: 'flutter-study-jam-chat.firebaseapp.com',
-        storageBucket: 'flutter-study-jam-chat.appspot.com',
+        appId: '1:495369196783:web:e6fcda561e827d0e6b771a',
+        messagingSenderId: '495369196783',
+        projectId: 'flutter-study-jam-chat-f6f94',
+        authDomain: 'flutter-study-jam-chat-f6f94.firebaseapp.com',
+        storageBucket: 'flutter-study-jam-chat-f6f94.appspot.com',
       );
 
   static FirebaseOptions android(String apiKey) => FirebaseOptions(
         apiKey: apiKey,
-        appId: '1:733105866417:android:3c79422507496dca42c490',
-        messagingSenderId: '733105866417',
-        projectId: 'flutter-study-jam-chat',
-        storageBucket: 'flutter-study-jam-chat.appspot.com',
+        appId: '1:495369196783:android:7f0925fb1b46a0206b771a',
+        messagingSenderId: '495369196783',
+        projectId: 'flutter-study-jam-chat-f6f94',
+        storageBucket: 'flutter-study-jam-chat-f6f94.appspot.com',
       );
 
   static FirebaseOptions ios(String apiKey) => FirebaseOptions(
         apiKey: apiKey,
-        appId: '1:733105866417:ios:c7bd9f729a6c6aeb42c490',
-        messagingSenderId: '733105866417',
-        projectId: 'flutter-study-jam-chat',
-        storageBucket: 'flutter-study-jam-chat.appspot.com',
+        appId: '1:495369196783:ios:c2e9c649906e79166b771a',
+        messagingSenderId: '495369196783',
+        projectId: 'flutter-study-jam-chat-f6f94',
+        storageBucket: 'flutter-study-jam-chat-f6f94.appspot.com',
         iosClientId:
-            '733105866417-b913tlsk16lvctktgpia2nrejljo91f3.apps.googleusercontent.com',
+            '495369196783-qj1a0ko1bmn2p0br6v4hn2dgs7nif9la.apps.googleusercontent.com',
         iosBundleId: 'com.example.surfPracticeChatFlutter',
       );
 }
